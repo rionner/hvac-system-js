cf95c47f37374b7915069fdbe130bad76cbc92ee
Eric Smith <esmith@8thlight.com>
2018-09-14 09:27:19 -0500

    Add a solution
    Let's get this reviewed

--- app/js/environment-controller.js
+++ app/js/environment-controller.js
@@ -1,6 +1,90 @@
-EnvironmentController = function() {
-  this.tick = function() {
+// I did this all es5 style, moment of whimsy. Don't take this to imply I normally don't use modules or classes.
 
+HvacWrapper = function(hvac) {
+  this.temp = function() {
+    return hvac.getTemp();
   };
+
+  this.turnOnHeat = function() {
+    hvac.setHeat(true);
+  };
+
+  this.turnOffHeat = function() {
+    hvac.setHeat(false);
+  };
+
+  this.turnOnAir = function() {
+    hvac.setCool(true);
+  };
+
+  this.turnOffAir = function() {
+    hvac.setCool(false);
+  };
+
+  this.turnOnFan = function() {
+    hvac.setFan(true);
+  };
+
+  this.turnOffFan = function() {
+    hvac.setFan(false);
+  };
+
+  return this;
+};
+
+EnvironmentController = function(hvac) {
+  const HEAT_SLEEP_TIME = 5;
+  const AIR_SLEEP_TIME = 3;
+  const MINIMUM_TEMP = 65;
+  const MAX_TEMP = 75;
+
+  this._sleepRemaining = 0;
+  this._previousTemp = -Number.MAX_SAFE_INTEGER;
+
+  this.tick = function() {
+    if (this.isSleeping()) {
+      this.sleepAnotherMinute();
+    } else {
+      if (hvac.temp() < MINIMUM_TEMP && !this.wasCooling())  {
+        hvac.turnOnHeat();
+        hvac.turnOnFan();
+      } else if (hvac.temp() > MAX_TEMP && !this.wasHeating()) {
+        hvac.turnOnAir();
+        hvac.turnOnFan();
+      } else {
+        hvac.turnOffFan();
+        hvac.turnOffAir();
+        hvac.turnOffHeat();
+        this.startSleeping();
+      }
+      this._previousTemp = hvac.temp();
+    }
+  };
+
+  this.isSleeping = function() {
+    return this._sleepRemaining != 0;
+  };
+
+  this.sleepAnotherMinute = function() {
+    this._sleepRemaining--;
+  };
+
+  this.wasHeating = function() {
+    return this._previousTemp != -Number.MAX_SAFE_INTEGER &&
+        this._previousTemp < MINIMUM_TEMP;
+  };
+
+  this.wasCooling = function() {
+    return this._previousTemp > MAX_TEMP;
+  };
+
+  this.startSleeping = function() {
+    if (this.wasHeating())  {
+      this._sleepRemaining = HEAT_SLEEP_TIME;
+    } else if (this.wasCooling()) {
+      this._sleepRemaining = AIR_SLEEP_TIME;
+    }
+  };
+
   return this;
 };
--- spec/environment-controller-spec.js
+++ spec/environment-controller-spec.js
@@ -1,5 +1,6 @@
 describe("environmentController", function() {
 
+  // Note this now ignored
   let hvacDummy = {
     _temp: 0,
     _heatOn: false,
@@ -11,9 +12,157 @@ describe("environmentController", function() {
     setFan(on) { this._fanOn = on; }
   };
 
-  it ("ticks", () => {
-    controller = new EnvironmentController(hvacDummy);
+  // This is actually the Mock. Interestingly this creates a wider interface, but this is actually the exception not the rule
+  let fakeHvacWrapper = {
+    temp() { return this._temp; },
+    turnOnHeat() { this._heatOn = true; },
+    turnOnFan() { this._fanOn = true; },
+    turnOnAir() { this._coolOn = true; },
+
+    turnOffFan() { this._fanOn = false; },
+    turnOffAir() { this._coolOn = false; },
+    turnOffHeat() { this._heatOn = false; },
+
+    // helper functions for testing
+    init() {
+      this._temp = 0;
+      this._heatOn = false;
+      this._coolOn = false;
+      this._fanOn = false;
+    },
+    isOff() { return !(this._heatOn || this._fanOn || this._coolOn); },
+    isHeating() { return this._heatOn && this._fanOn && !this._coolOn; },
+    isCooling() { return this._coolOn && this._fanOn && !this._heatOn; }
+  };
+
+  let controller;
+
+  beforeEach(() => {
+    fakeHvacWrapper.init();
+    controller = new EnvironmentController(fakeHvacWrapper);
+  });
+
+  it ("does nothing at the perfect temperature", () => {
+    fakeHvacWrapper._temp = 70;
 
     controller.tick();
+
+    expect(fakeHvacWrapper.isOff()).toBeTruthy();
+  });
+
+  it ("turns on the heat at 64", () => {
+    fakeHvacWrapper._temp = 64;
+
+    controller.tick();
+
+    expect(fakeHvacWrapper.isHeating()).toBeTruthy();
+  });
+
+  it ("does not turn on the heat at 65", () => {
+    fakeHvacWrapper._temp = 65;
+
+    controller.tick();
+
+    expect(fakeHvacWrapper.isOff()).toBeTruthy();
+  });
+
+  it("turns on the air at 76", () => {
+    fakeHvacWrapper._temp = 76;
+
+    controller.tick();
+
+    expect(fakeHvacWrapper.isCooling()).toBeTruthy();
+  });
+
+  it("doesn't turn on the air at 75", () => {
+    fakeHvacWrapper._temp = 75;
+
+    controller.tick();
+
+    expect(fakeHvacWrapper.isOff()).toBeTruthy();
+  });
+
+  it("explicitly turns the air off at 75", () => {
+    fakeHvacWrapper._temp = 75;
+    fakeHvacWrapper._fanOn = true;
+    fakeHvacWrapper._coolOn = true;
+
+    controller.tick();
+
+    expect(fakeHvacWrapper.isOff()).toBeTruthy();
+  });
+
+  it("explicitly turns the heat off at 65", () => {
+    fakeHvacWrapper._temp = 65;
+    fakeHvacWrapper._heatOn = true;
+    fakeHvacWrapper._fanOn = true;
+
+    controller.tick();
+
+    expect(fakeHvacWrapper.isOff()).toBeTruthy();
+  });
+
+  it("does not turn on the fan right after turning off the heater", () => {
+    fakeHvacWrapper._temp = 64;
+    controller.tick();
+    fakeHvacWrapper._temp = 65;
+    controller.tick();
+
+    fakeHvacWrapper._temp = 64;
+    controller.tick();
+
+    expect(fakeHvacWrapper.isOff()).toBeTruthy();
+  });
+
+  it("waits for 5 minutes to turn the fan on after turning the heater off ", () => {
+    fakeHvacWrapper._temp = 64;
+    controller.tick();
+    fakeHvacWrapper._temp = 65;
+    controller.tick();
+
+    fakeHvacWrapper._temp = 64;
+    controller.tick();
+    controller.tick();
+    controller.tick();
+    controller.tick();
+    controller.tick();
+    expect(fakeHvacWrapper.isOff()).toBeTruthy();
+
+    controller.tick();
+    expect(fakeHvacWrapper.isHeating()).toBeTruthy();
+  });
+
+  it("only waits for 3 min after turning off the air", () => {
+    fakeHvacWrapper._temp = 76;
+    controller.tick();
+    fakeHvacWrapper._temp = 75;
+    controller.tick();
+
+    fakeHvacWrapper._temp = 76;
+    controller.tick();
+    controller.tick();
+    controller.tick();
+    expect(fakeHvacWrapper.isOff()).toBeTruthy();
+
+    controller.tick();
+    expect(fakeHvacWrapper.isCooling()).toBeTruthy();
+  });
+
+  it("turns off everything in the event of spike in heat", () => {
+    fakeHvacWrapper._temp = 64;
+    controller.tick();
+    fakeHvacWrapper._temp = 76;
+    controller.tick();
+
+    expect(fakeHvacWrapper.isOff()).toBeTruthy();
+  });
+
+  it("turns off everything in the event of a sudden drop in temperature", () => {
+    fakeHvacWrapper._temp = 76;
+    controller.tick();
+    fakeHvacWrapper._temp = 64;
+    controller.tick();
+
+    expect(fakeHvacWrapper.isOff()).toBeTruthy();
   });
 });
